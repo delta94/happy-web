0ca8733d1ec80f8b282add4523f6e4b61d9d2044
unknown <viniciusbeloti7@gmail.com>
2020-10-12 08:08:48 -0300

    chore: add landing styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import './styles/global.css';
+import './styles/pages/landing.css';
 
 function App() {
   return (
-    <div>
+    <div id="page-landing">
       Hello World
     </div>
   );
