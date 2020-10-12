f027cff93f00d0e5a12558fd918d8105a1c6ff36
unknown <viniciusbeloti7@gmail.com>
2020-10-12 08:22:48 -0300

    chore: add react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React from "react";
+import { FiArrowRight } from 'react-icons/fi'
 
-import './styles/global.css';
-import './styles/pages/landing.css';
+import "./styles/global.css";
+import "./styles/pages/landing.css";
+
+import logoImg from "./images/logo.svg";
 
 function App() {
   return (
     <div id="page-landing">
-      Hello World
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Logo Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+
+          <div className="location">
+            <strong>Sorocaba</strong>
+            <span>São Paulo</span>
+          </div>
+
+          <a href="/" className="enter-app">
+            <FiArrowRight />
+          </a>
+        </main>
+      </div>
     </div>
   );
 }
