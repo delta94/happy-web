dc9a6972679445720d9080b47fd1083184624d8b
unknown <viniciusbeloti7@gmail.com>
2020-10-12 08:43:29 -0300

    chore: add orphanages-map  styles

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
-  return <h1>Hello</h1>;
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Sorocaba</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;
