e5c3d5344404904b448acd2112fb47061b3930a9
unknown <viniciusbeloti7@gmail.com>
2020-10-14 09:04:13 -0300

    refactor: move mapIcon to file

--- src/pages/CreateOrphanage.tsx
+++ src/pages/CreateOrphanage.tsx
@@ -1,26 +1,16 @@
-import React from "react";
+import React from 'react';
 import { Map, Marker, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
+import { FiPlus } from 'react-icons/fi';
 
-import Sidebar from "../components/Sidebar";
+import Sidebar from '../components/Sidebar';
+import mapIcon from '../utils/mapIcon';
 
-import { FiPlus } from "react-icons/fi";
-import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/create-orphanage.css';
 
-const happyMapIcon = L.icon({
-  iconUrl: mapMarkerImg,
-
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60]
-})
-
 export default function CreateOrphanage() {
-
   return (
     <div id="page-create-orphanage">
-        <Sidebar />
+      <Sidebar />
 
       <main>
         <form className="create-orphanage-form">
@@ -28,7 +18,7 @@ export default function CreateOrphanage() {
             <legend>Dados</legend>
 
             <Map
-              center={[-27.2092052,-49.6401092]}
+              center={[-27.2092052, -49.6401092]}
               style={{ width: '100%', height: 280 }}
               zoom={15}
             >
@@ -36,7 +26,11 @@ export default function CreateOrphanage() {
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
 
-              <Marker interactive={false} icon={happyMapIcon} position={[-27.2092052,-49.6401092]} />
+              <Marker
+                interactive={false}
+                icon={mapIcon}
+                position={[-27.2092052, -49.6401092]}
+              />
             </Map>
 
             <div className="input-block">
@@ -45,16 +39,16 @@ export default function CreateOrphanage() {
             </div>
 
             <div className="input-block">
-              <label htmlFor="about">Sobre <span>Máximo de 300 caracteres</span></label>
+              <label htmlFor="about">
+                Sobre <span>Máximo de 300 caracteres</span>
+              </label>
               <textarea id="name" maxLength={300} />
             </div>
 
             <div className="input-block">
               <label htmlFor="images">Fotos</label>
 
-              <div className="uploaded-image">
-
-              </div>
+              <div className="uploaded-image"></div>
 
               <button className="new-image">
                 <FiPlus size={24} color="#15b6d6" />
@@ -79,7 +73,9 @@ export default function CreateOrphanage() {
               <label htmlFor="open_on_weekends">Atende fim de semana</label>
 
               <div className="button-select">
-                <button type="button" className="active">Sim</button>
+                <button type="button" className="active">
+                  Sim
+                </button>
                 <button type="button">Não</button>
               </div>
             </div>
